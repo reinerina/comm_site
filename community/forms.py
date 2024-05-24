2f4dc05dc0429ae4a68bb182af75edd2769f42ef
ve1i0na <1907615595@qq.com>
2024-05-24 11:43:14 +0800

    feat:add more items

--- community/forms.py
+++ community/forms.py
@@ -4,8 +4,10 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'content']
+        fields = ['title', 'content', 'task', 'task_time']
         labels = {
             'title': '标题',
             'content': '内容',
+            'task': '任务',
+            'task_time': '任务时间'
         }
--- community/models.py
+++ community/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    task = models.CharField(max_length=200, default='')
+    task_time = models.CharField(max_length=200, default='')
 
     def __str__(self):
         return self.title
